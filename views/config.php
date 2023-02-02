cd0e0ea03cdd46741465e311c7ee6274bc1ac47c
Sara Marrero <saramarreromiranda@gmail.com>
2023-02-02 13:11:42 +0000

    avance

--- views/config.php
+++ views/config.php
@@ -9,7 +9,7 @@
             'host' => $_ENV['DB_HOST'],
             'user' => $_ENV['DB_USER'],
             'pass' => $_ENV['DB_PASS'],
-            'name' => $_ENV['DB_NAME'],
+            'name' => $_ENV['DB_DB'],
             'options' => [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
